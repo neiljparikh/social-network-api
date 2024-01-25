e0c298b2450b0577975737ac40b7bc01858618d0
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-25 15:29:05 -0500

    Built out server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//gives you the ability to open the DB on server open
+const db = require('./config/connection');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
